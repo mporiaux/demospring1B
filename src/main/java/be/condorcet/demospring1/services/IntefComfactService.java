0c4d2918f5ecf4064827bf4b9fed50931e1d0afd
mporiaux <michel.poriaux@condorcet.be>
2023-10-18 10:44:49 +0200

    ajout des interfaces de service

---
+++ src/main/java/be/condorcet/demospring1/services/IntefComfactService.java
@@ -0,0 +1,10 @@
+package be.condorcet.demospring1.services;
+
+import be.condorcet.demospring1.modele.Client;
+import be.condorcet.demospring1.modele.Comfact;
+
+import java.util.List;
+
+public interface IntefComfactService extends InterfService<Comfact> {
+    public List<Comfact> getComfacts(Client cl);
+}
---
+++ src/main/java/be/condorcet/demospring1/services/InterfClientService.java
@@ -0,0 +1,9 @@
+package be.condorcet.demospring1.services;
+
+import be.condorcet.demospring1.modele.Client;
+
+import java.util.List;
+
+public interface InterfClientService extends InterfService<Client>{
+    public List<Client> read(String nom);
+}

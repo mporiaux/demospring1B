77c9f90f396d78e3edd90ac606d3becb5e839fe5
mporiaux <michel.poriaux@condorcet.be>
2023-10-25 10:20:23 +0200

    tests unitaires sur commandes

---
+++ src/test/java/be/condorcet/demospring1/services/ComfactServiceImplTest.java
@@ -0,0 +1,133 @@
+package be.condorcet.demospring1.services;
+
+import be.condorcet.demospring1.modele.Client;
+import be.condorcet.demospring1.modele.Comfact;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ComfactServiceImplTest {
+    private Client cl;
+    private Comfact cf;
+    @Autowired
+    private ClientServiceImpl clientServiceImpl;
+
+    @Autowired
+    private ComfactServiceImpl comfactServiceImpl;
+
+    @BeforeEach
+    void setUp() {
+        try{
+            cl = new Client(null,"NomTest","PrenomTest",1000,"LocTest","rue test","1","0001",new ArrayList<>());
+            clientServiceImpl.create(cl);
+            System.out.println("création du client : "+cl);
+            cf = new Comfact( Date.valueOf(LocalDate.now()),"c",new BigDecimal(1000),cl);
+            comfactServiceImpl.create(cf);
+            System.out.println("création de la commande : "+cf);
+        }
+        catch (Exception e){
+            System.out.println("erreur de création de commande "+e);
+        }
+    }
+
+    @AfterEach
+    void tearDown() {
+        try{
+            comfactServiceImpl.delete(cf);
+        }
+        catch(Exception e){
+            System.out.println("erreur d'effacement de commande "+e);
+        }
+        try{
+            clientServiceImpl.delete(cl);
+        }
+        catch(Exception e){
+            System.out.println("erreur d'effacement de client "+e);
+        }
+    }
+
+
+    @Test
+    void create() {
+
+        assertNotEquals(0,cf.getIdcommande(),"numéro de commande non incrémenté");
+        //tester date et montant
+    }
+
+
+
+    @Test
+    void read() {
+        try{
+            int numc=cf.getIdcommande();
+            Comfact co2=comfactServiceImpl.read(numc);
+            assertEquals(new BigDecimal(1000.00).setScale(2,RoundingMode.HALF_UP),co2.getMontant().setScale(2,RoundingMode.HALF_UP),"montants différents "+new BigDecimal(1000).setScale(2,RoundingMode.HALF_UP)+"-"+co2.getMontant().setScale(2,RoundingMode.HALF_UP));
+            //etc
+        }
+        catch (Exception e){
+            fail("recherche infructueuse "+e);
+        }
+    }
+
+    @Test
+    void update() {
+        //etc
+        try{
+
+            cf.setMontant(new BigDecimal(50.25).setScale(2,RoundingMode.HALF_UP));
+            cf= comfactServiceImpl.update(cf);
+            assertEquals(cf.getMontant(),new BigDecimal(50.25).setScale(2, RoundingMode.HALF_UP),"montants différents "+cf.getMontant()+"-"+new BigDecimal(50.25));
+            //etc
+
+        }
+        catch(Exception e){
+            fail("erreur de mise à jour "+e);
+        }
+    }
+
+    @Test
+    void delete() {
+        try{
+            comfactServiceImpl.delete(cf);
+            Assertions.assertThrows(Exception.class, () -> {
+                comfactServiceImpl.read(cf.getIdcommande());
+            },"record non effacé");
+        }
+        catch(Exception e){
+            fail("erreur d'effacement "+e);
+        }
+    }
+
+    @Test
+    void affCollection(){ //ajouté
+        try {
+            Collection<Comfact> lco = comfactServiceImpl.getComfacts(cl);
+            boolean trouve = false;
+            for(Comfact c:lco){
+                if(c.getIdcommande().equals(cf.getIdcommande())){
+                    trouve=true;
+                    break;
+                }
+            }
+            assertTrue(trouve,"commande absente de la liste du client");
+        }
+        catch(Exception e){
+            fail("Erreur de recherche "+e);
+        }
+    }
+}
+
+
